b9806f5c85d092623c624d4df7232c420b4a69be
inazrabuu <inazrabuu@gmail.com>
2025-04-19 14:05:00 +0700

    created: app text dictionary

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,8 @@
+class AppStrings {
+  static const appName = 'Arra Jewelry ID';
+  static const appTitle = 'ARRA';
+  static const welcomeMessage = 'Welcome to Arra Jewelry ID';
+  static const homePageTitle = 'Home';
+  static const productPageTitle = 'Product';
+  static const transactionPageTitle = 'Transaction';
+}
