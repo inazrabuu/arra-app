32b61eb0779f0fc404a6f5dd2dc9f9f2c792d0f8
inazrabuu <inazrabuu@gmail.com>
2025-04-19 14:18:29 +0700

    edit: restructure dictionary

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -1,8 +1,34 @@
 class AppStrings {
   static const appName = 'Arra Jewelry ID';
-  static const appTitle = 'ARRA';
-  static const welcomeMessage = 'Welcome to Arra Jewelry ID';
-  static const homePageTitle = 'Home';
-  static const productPageTitle = 'Product';
-  static const transactionPageTitle = 'Transaction';
+
+  static const app = _AppStrings();
+  static const home = _HomeStrings();
+  static const product = _ProductStrings();
+  static const transaction = _TransactionStrings();
+}
+
+class _AppStrings {
+  const _AppStrings();
+
+  final name = 'Arra Jewelry ID';
+  final title = 'ARRA';
+  final welcome = 'Welcome to Arra Jewelry ID';
+}
+
+class _HomeStrings {
+  const _HomeStrings();
+
+  final title = 'Home';
+}
+
+class _ProductStrings {
+  const _ProductStrings();
+
+  final title = 'Products';
+}
+
+class _TransactionStrings {
+  const _TransactionStrings();
+
+  final title = 'Transactions';
 }
