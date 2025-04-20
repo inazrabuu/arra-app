d047aa289969dab8e82dafcb83f76c5075219c38
inazrabuu <inazrabuu@gmail.com>
2025-04-20 13:53:36 +0700

    add new property

--- lib/views/theme/text_styles.dart
+++ lib/views/theme/text_styles.dart
@@ -11,4 +11,19 @@ class AppText {
       ),
     );
   }
+
+  static Widget gridTitle(String text, {Color? color}) {
+    return Text(text, style: TextStyle(fontSize: 16));
+  }
+
+  static Widget gridPrice(String text, {Color? color}) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w700,
+        color: color ?? Colors.black,
+      ),
+    );
+  }
 }
