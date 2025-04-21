3638c4dcdaf9ec678b69dd9de3e4e3ac20cf263b
inazrabuu <inazrabuu@gmail.com>
2025-04-21 07:33:36 +0700

    added new styles

--- lib/views/theme/text_styles.dart
+++ lib/views/theme/text_styles.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AppText {
+  static Widget small(String text) {
+    return Text(text, style: TextStyle(fontSize: 12));
+  }
+
+  static Widget bold(String text) {
+    return Text(text, style: TextStyle(fontWeight: FontWeight.bold));
+  }
+
   static Widget heading(String text, {Color? color}) {
     return Text(
       text,
       style: TextStyle(
         fontSize: 24,
         fontWeight: FontWeight.w700,
+        color: color ?? Colors.black,
+      ),
+    );
+  }
+
+  static Widget heading2(String text, {Color? color}) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
         color: color ?? Colors.black,
       ),
     );
