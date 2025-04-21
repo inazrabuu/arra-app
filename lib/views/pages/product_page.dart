df39d00b40f4e5a4b37c24470c43fc023240095f
inazrabuu <inazrabuu@gmail.com>
2025-04-21 07:33:26 +0700

    moved data to dummies

--- lib/views/pages/product_page.dart
+++ lib/views/pages/product_page.dart
@@ -1,17 +1,11 @@
 import 'package:arrajewelry/constants/app_strings.dart';
+import 'package:arrajewelry/data/dummies.dart';
 import 'package:arrajewelry/views/theme/text_styles.dart';
 import 'package:arrajewelry/views/widgets/products_grid_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:basic_utils/basic_utils.dart';
 
 class ProductPage extends StatelessWidget {
-  static const List<Map<String, dynamic>> items = [
-    {'cat': 'anting', 'name': "chandraprabha", "price": "150.000"},
-    {'cat': 'gelang', 'name': "sringai", "price": "150.000"},
-    {'cat': 'kalung', 'name': "leela", "price": "150.000"},
-    {'cat': "cincin", 'name': 'vagni', "price": "150.000"},
-    {'cat': "anting", 'name': 'lontong', "price": "150.000"},
-  ];
+  static final List<Map<String, dynamic>> items = DataDummies.products;
 
   const ProductPage({super.key});
 
