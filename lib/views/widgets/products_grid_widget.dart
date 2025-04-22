9058b4cbc09fedb2c88732ae5a71bd64eae75b18
inazrabuu <inazrabuu@gmail.com>
2025-04-22 20:07:59 +0700

    edited implement live products

--- lib/views/widgets/products_grid_widget.dart
+++ lib/views/widgets/products_grid_widget.dart
@@ -1,9 +1,13 @@
+import 'package:arrajewelry/models/product_model.dart';
+import 'package:arrajewelry/utils/helpers.dart';
+import 'package:arrajewelry/views/widgets/image_cloud.dart';
 import 'package:flutter/material.dart';
 import 'package:arrajewelry/views/theme/text_styles.dart';
 import 'package:basic_utils/basic_utils.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ProductsGridWidget extends StatelessWidget {
-  final List<Map<String, dynamic>> items;
+  final List<ProductModel> items;
 
   const ProductsGridWidget({super.key, required this.items});
 
@@ -18,7 +22,7 @@ class ProductsGridWidget extends StatelessWidget {
         crossAxisCount: 2,
         mainAxisSpacing: 0,
         crossAxisSpacing: 0,
-        childAspectRatio: 8 / 10,
+        childAspectRatio: 0.68,
       ),
       itemBuilder: (context, index) {
         return Padding(
@@ -29,30 +33,51 @@ class ProductsGridWidget extends StatelessWidget {
               color: const Color(0xFFF6F6F6),
             ),
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(4, 16, 4, 0),
+              padding: const EdgeInsets.all(0),
               child: Column(
                 children: [
                   Center(
-                    child: Image.asset(
-                      "assets/images/products/${index + 1}.png",
-                      width: 100.0,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16),
+                      ),
+                      child: ImageCloud(
+                        path:
+                            "${dotenv.env['BUCKET_PRODUCT']}/${items[index].image}",
+                        bucket: dotenv.env['SUPABASE_BUCKET']!,
+                        width: 175,
+                        height: 175,
+                      ),
                     ),
                   ),
                   Center(
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-                      child: Text(StringUtils.capitalize(items[index]['cat'])),
+                      child: Text(
+                        StringUtils.capitalize(
+                          items[index].cat,
+                          allWords: true,
+                        ),
+                      ),
                     ),
                   ),
                   Center(
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(8, 0, 8, 4),
                       child: AppText.gridTitle(
-                        StringUtils.capitalize(items[index]['name']),
+                        StringUtils.capitalize(
+                          items[index].name,
+                          allWords: true,
+                        ),
                       ),
                     ),
                   ),
-                  Center(child: AppText.gridPrice("${items[index]['price']}")),
+                  Center(
+                    child: AppText.gridPrice(
+                      Helpers.priceIdrFormat(items[index].price),
+                    ),
+                  ),
                 ],
               ),
             ),
