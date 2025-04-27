88951614e3fe914b4b32ac8b81dbbfca2429913d
inazrabuu <inazrabuu@gmail.com>
2025-04-27 21:31:10 +0700

    created transaction detail widget

---
+++ lib/views/widgets/transaction_detail.dart
@@ -0,0 +1,220 @@
+import 'package:arrajewelry/constants/candy_colors.dart';
+import 'package:arrajewelry/models/transaction_model.dart';
+import 'package:arrajewelry/views/theme/text_styles.dart';
+import 'package:basic_utils/basic_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionDetailWidget extends StatelessWidget {
+  final TransactionModel transaction;
+
+  const TransactionDetailWidget({super.key, required this.transaction});
+
+  Icon boolIcon(bool value) {
+    return value
+        ? Icon(Icons.check_rounded, color: CandyColors.colors['green'])
+        : Icon(Icons.close_rounded, color: CandyColors.colors['red']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String formattedDate = DateFormat('MMM d, y').format(transaction.trxDate);
+
+    final idrFormat = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: 'Rp',
+      decimalDigits: 0,
+    );
+    String total = idrFormat.format(transaction.total);
+
+    dynamic debit = {
+      "text": transaction.isDebit ? 'DEBIT' : 'CREDIT',
+      "color":
+          transaction.isDebit
+              ? CandyColors.colors['red']
+              : CandyColors.colors['green'],
+    };
+
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 24),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 8,
+                      child: AppText.heading2(
+                        'Transaction #${transaction.orderNo}',
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            constraints: const BoxConstraints(minWidth: 40),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 4,
+                              horizontal: 8,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: debit['color'],
+                                width: 1,
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                debit['text'],
+                                style: TextStyle(
+                                  color: debit['color'],
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(color: Colors.grey[350]),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Row(
+                        children: [
+                          boolIcon(transaction.isPaid),
+                          SizedBox(width: 4),
+                          AppText.bold('Paid'),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Row(
+                        children: [
+                          boolIcon(transaction.isFulfilled),
+                          SizedBox(width: 4),
+                          AppText.bold('Fulfilled'),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Icon(Icons.content_copy_rounded),
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(color: Colors.grey[350]),
+                Row(
+                  children: [
+                    Expanded(flex: 4, child: AppText.bold('Date')),
+                    Expanded(flex: 6, child: Text(formattedDate)),
+                  ],
+                ),
+                Divider(color: Colors.grey[350]),
+                Row(
+                  children: [
+                    Expanded(flex: 4, child: AppText.bold('Name')),
+                    Expanded(flex: 6, child: Text(transaction.name)),
+                  ],
+                ),
+                Divider(color: Colors.grey[350]),
+                Row(
+                  children: [
+                    Expanded(flex: 4, child: AppText.bold('Description')),
+                    Expanded(flex: 6, child: Text(transaction.description)),
+                  ],
+                ),
+                Divider(color: Colors.grey[350]),
+                Row(
+                  children: [
+                    Expanded(flex: 4, child: AppText.bold('Total')),
+                    Expanded(flex: 6, child: AppText.gridPrice(total)),
+                  ],
+                ),
+                SizedBox(height: 8),
+                Container(
+                  constraints: BoxConstraints(maxHeight: 150),
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.grey[400]!, width: 1),
+                  ),
+                  child: ListView.builder(
+                    itemCount: transaction.detail.length,
+                    itemBuilder: (context, index) {
+                      String item = StringUtils.capitalize(
+                        transaction.detail[index]['item'],
+                        allWords: true,
+                      );
+                      String qty = transaction.detail[index]['qty'].toString();
+                      return Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 0,
+                              horizontal: 8.0,
+                            ),
+                            child: Row(
+                              children: [
+                                Expanded(child: Text(item)),
+                                Expanded(child: Center(child: Text(qty))),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                        ],
+                      );
+                    },
+                  ),
+                  // child: Column(
+                  //   children: [
+                  //     ...transaction.detail.asMap().entries.map((e) {
+                  //       String item = StringUtils.capitalize(
+                  //         e.value['item'],
+                  //         allWords: true,
+                  //       );
+                  //       String qty = e.value['qty'].toString();
+                  //       return Column(
+                  //         children: [
+                  //           Padding(
+                  //             padding: const EdgeInsets.symmetric(
+                  //               vertical: 0,
+                  //               horizontal: 8.0,
+                  //             ),
+                  //             child: Row(
+                  //               children: [
+                  //                 Expanded(child: Text(item)),
+                  //                 Expanded(child: Center(child: Text(qty))),
+                  //               ],
+                  //             ),
+                  //           ),
+                  //           SizedBox(height: 4),
+                  //         ],
+                  //       );
+                  //     }),
+                  //   ],
+                  // ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
