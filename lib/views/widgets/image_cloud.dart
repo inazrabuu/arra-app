71bcf6b22cb6f2a5ea716b5c87908ad45af9bcea
inazrabuu <inazrabuu@gmail.com>
2025-04-27 16:21:23 +0700

    remove dependency

--- lib/views/widgets/image_cloud.dart
+++ lib/views/widgets/image_cloud.dart
@@ -1,7 +1,6 @@
 import 'package:arrajewelry/data/image_cache.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ImageCloud extends StatelessWidget {
   final String path;
